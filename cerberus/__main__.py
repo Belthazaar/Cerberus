717f3a39bf6790b6b3c3b1b7f12168456344813b
Christoff Visser <christoffvis@gmail.com>
2022-06-03 11:34:57 +0900

    Add more ryu args

--- cerberus/__main__.py
+++ cerberus/__main__.py
@@ -40,19 +40,40 @@ def parse_args(sys_args):
         action='store_true',
         help='Runs in background with no output'
     )
-    args.add_argument(
+    ryu_args = args.add_argument_group('ryu-manager arguments')
+    ryu_args.add_argument(
         '--wsapi-host',
         dest='wsapi_host',
         action='store',
         help="webapp listen host (default 0.0.0.0)"
     )
-    args.add_argument(
+    ryu_args.add_argument(
         '--wsapi-port',
         dest='wsapi_port',
         action='store',
         help="webapp listen host (default 8080)"
     )
-
+    ryu_args.add_argument(
+        '--ofp-listen-host',
+        dest='ofp_host',
+        action='store',
+        help="OpenFlow listen host (default 0.0.0.0)",
+        default='0.0.0.0'
+    )
+    ryu_args.add_argument(
+        '--ofp-ssl-listen-port',
+        dest='ofp_ssl_port',
+        action='store',
+        help="OpenFlow SSL listen port (default 6653)",
+        default=6653
+    )
+    ryu_args.add_argument(
+        '--ofp-tcp-listen-port',
+        dest='ofp_tcp_port',
+        action='store',
+        help="OpenFlow listen port (default 6653)",
+        default=6653
+    )
     return args.parse_args(sys_args)
 
 
@@ -73,6 +94,12 @@ def main():
        ryu_args.extend(["--wsapi-host", args.wsapi_host])
     if args.wsapi_port:
         ryu_args.extend(["--wsapi-port", args.wsapi_port])
+    if args.ofp_host:
+        ryu_args.extend(["--ofp-listen-host", args.ofp_host])
+    if args.ofp_tcp_port:
+        ryu_args.extend(["--ofp-tcp-listen-port", args.ofp_tcp_port])
+    if args.ofp_ssl_port:
+        ryu_args.extend(["--ofp-ssl-listen-port", args.ofp_ssl_port])
 
     os.execvp('ryu-manager', ['ryu-manager', *ryu_args ,'cerberus.cerberus'])
 
